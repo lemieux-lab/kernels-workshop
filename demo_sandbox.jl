7f82bee01e692cb4f2154e71e9e807b45fbb90bd
Carl Munoz <57541061+CarlMunoz1029@users.noreply.github.com>
2026-02-06 16:47:23 -0500

    Update demo_sandbox.jl

--- demo_sandbox.jl
+++ demo_sandbox.jl
@@ -40,7 +40,8 @@ end
 
 
 
-#! 5x faster, unstable, no maximum subtraction 
+# On block per column, a chunk of rows per thread
+#! 5x faster than previous kernel, unstable, no maximum subtraction
 function softmax_kernel(input::CuDeviceArray{T}, sumsi::CuDeviceArray{T}, acc::CuDeviceArray{T}) where {T}
     # Thread index
     chunk_size = size(input,1) ÷ blockDim().x #! Only cuz the size is good
@@ -49,42 +50,42 @@ function softmax_kernel(input::CuDeviceArray{T}, sumsi::CuDeviceArray{T}, acc::C
     base_i = i * chunk_size
     j = (blockIdx().y - 1)* blockDim().y + threadIdx().y
 
-    #chunk_sums = CuStaticSharedArray(T, 1024)
-    for idx in 1:chunk_size
-        acc[idx + base_i,j] = exp(input[idx + base_i,j])    
-        #chunk_sums[i+1] += acc[idx + base_i,j] 
-        sumsi[i+1,j] += acc[idx + base_i,j]         
-    end 
-    
-    sync_threads()
-    
-    if i==0
-        sumsi[1,j] = sum(@view sumsi[:,j])
+    if j <= size(input,2)
+        #! Unfinished attempt with shared arrays, not needed
+        # chunk_sums = CuStaticSharedArray(T, 1024)
+        for idx in 1:chunk_size
+            if idx + base_i <= size(input, 1)
+                acc[idx + base_i,j] = exp(input[idx + base_i,j])    
+                # chunk_sums[i+1] += acc[idx + base_i,j] 
+                sumsi[i+1,j] += acc[idx + base_i,j]         
+            end
+        end 
+        
+        sync_threads()
+        
+        if i==0
+            sumsi[1,j] = sum(@view sumsi[:,j])
+        end
+        
+        sync_threads()
+        
+        for idx in 1:chunk_size
+            acc[idx+base_i,j] /= sumsi[1,j]
+        end
     end
-    
-    sync_threads()
-    
-    for idx in 1:chunk_size
-        acc[idx+base_i,j] /= sumsi[1,j]
-    end
-    
     return nothing    
 end
 
-v1 =  cu(randn(1024*20,1024*10))
+v1 =  cu(randn(20000,10000))
 
 threads_per_block = (1024, 1) # 1024
 blocks_per_grid = (cld(1, threads_per_block[1]), cld(size(v1, 2),threads_per_block[2]))
 
-sums = CUDA.zeros(1024, 10240)
-accs = CUDA.zeros(size(v1))
-
-@btime begin	
+@btime begin
+    sums = CUDA.zeros(1024, 10240)
+    accs = CUDA.zeros(size(v1))	
 	@cuda threads=threads_per_block blocks=blocks_per_grid softmax_kernel(v1, sums, accs)
-	# Grid-synced division, kind of cheating. 
-	#res_sm = accs./sums[:,:]'
 end
-
 sums
 
 sum(accs,dims=1)
